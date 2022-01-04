c527562ce2fb07e2aa2820ef738ace274f028ffd
WEIzone082 <wtfuhell@gmail.com>
2022-01-04 11:00:09 +0800

    Fucked up pagination. Shit ain't working

--- src/components/Data_Footer.vue
+++ src/components/Data_Footer.vue
@@ -33,50 +33,51 @@
 
 <script>
 export default {
-  props: ["count", "DPP", "start", "end", "pages"],
-  data() {
-    return {
-      AvailPages: [],
-    };
-  },
-  methods: {
-    PageInitialize: function () {
-      if (this.count >= 3) {
-        this.AvailPages = [1, 2, 3];
-      } else {
-        for (let i = 1; i <= this.pages; i++) {
-          this.AvailPages.push(i);
-        }
-      }
-    },
-    pageChanger: function (page) {
-      if (page === "prev") {
-        if (this.start !== 1) {
-          this.AvailPages.pop;
-          this.AvailPages.unshifit(Math.min(this.AvailPages) - 1);
-          let num = Math.min(this.AvailPages) * this.DPP;
-          this.$emit("changeData", num, num + this.DPP + 1);
-        }
-      } else if (page === "next") {
-        if (this.end !== this.count) {
-          this.AvailPages.shift;
-          this.AvailPages.push(Math.max(this.AvailPages) + 1);
-          let num = Math.max(this.AvailPages) * this.DPP;
-          this.$emit("changeData", num, num + this.DPP + 1);
-        }
-      } else {
-        console.log("triggered!");
-        this.$emit(
-          "changeData",
-          page * this.DPP,
-          page * this.DPP + this.DPP + 1
-        );
-      }
-    },
-  },
-  beforeUpdate: function () {
-    this.PageInitialize();
-  },
+  // props: ["count", "DPP", "start", "end", "pages"],
+  // data() {
+  //   return {
+  //     AvailPages: [],
+  //   };
+  // },
+  // methods: {
+  //   PageInitialize: function () {
+  //     if (this.count >= 3) {
+  //       this.AvailPages = [1, 2, 3];
+  //     } else {
+  //       for (let i = 1; i <= this.pages; i++) {
+  //         this.AvailPages.push(i);
+  //       }
+  //     }
+  //   },
+  //   pageChanger: function (page) {
+  //     if (page === "prev") {
+  //       if (this.start !== 1) {
+  //         this.AvailPages.pop();
+  //         this.AvailPages.unshifit(Math.min(...this.AvailPages) - 1);
+  //         let num = Math.max(...this.AvailPages) * this.DPP;
+  //         this.$emit("changeData", { begin: num - this.DPP, end: num + 1 });
+  //       }
+  //     } else if (page === "next") {
+  //       if (this.end !== this.count) {
+  //         this.AvailPages.shift();
+  //         this.AvailPages.push(Math.max(...this.AvailPages) + 1);
+  //         console.log(this.AvailPages);
+  //         let num = Math.min(...this.AvailPages) * this.DPP;
+  //         this.$emit("changeData", { begin: num - this.DPP, end: num });
+  //       }
+  //     } else {
+  //       console.log("triggered!");
+  //       this.$emit(
+  //         "changeData",
+  //         page * this.DPP,
+  //         page * this.DPP + this.DPP + 1
+  //       );
+  //     }
+  //   },
+  // },
+  // beforeUpdate: function () {
+  //   this.PageInitialize();
+  // },
 };
 </script>
 
