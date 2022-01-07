7ab999d5945117bd8a9867adf89127bf9eaefe7b
shark <shark53210@gmail.com>
2022-01-07 15:45:35 +0800

    products up 2

--- src/store/index.js
+++ src/store/index.js
@@ -135,11 +135,97 @@ const member = {
 	}
 }
 
+const products = {
+	namespaced: true,
+	actions: {
+		// 顯示每項tr
+		displayAPI(context, apiPath){
+			// 取得PHP資料
+			return axios.get(`http://localhost:8080/api/yoshi/backend/${apiPath}`).then(
+				response => {
+					let tempData = {}
+					let arrData = []
+					// 只取要的資料
+					// 找陣列中每個art物件
+					for (const item of response.data) {
+	
+						// 去掉art物件中不需要的屬性(1~12)
+						for (const key in item) {
+							// 判斷是否為1~12，不是則加到新的物件中
+							if( !parseInt(key) && parseInt(key) !== 0){
+								tempData[key] = item[key]
+							}
+						}
+						// 再將該物件加入新陣列
+						arrData.unshift(tempData)
+						// 把暫存物件資料歸零
+						tempData = {}
+					}
+					// 將處理完的傳給mutations displayAPI方法
+					context.commit('displayAPI', arrData);
+				},
+				error => {
+					console.log(error.message);
+				}
+			)
+		},
+		// 刪除功能
+		deleteAPI(context, data){
+			return axios.post(`http://localhost:8080/api/yoshi/backend/${data.useAPI.deleteAPI}`, data.finalCheckedArr).then(
+				response => {
+					alert('刪除成功')
+				},
+				error => {
+					console.log(error.message);
+				}
+			)
+		},
+		// 上架功能
+		upStatusAPI(context, data){
+			return axios.post(`http://localhost:8080/api/yoshi/backend/${data.useAPI.upStatusAPI}`, data.finalCheckedArr).then(
+				response => {
+					alert('上架成功')
+				},
+				error => {
+					console.log(error.message);
+				}
+			)
+		},
+		// 下架架功能
+		downStatusAPI(context, data){
+			return axios.post(`http://localhost:8080/api/yoshi/backend/${data.useAPI.downStatusAPI}`, data.finalCheckedArr).then(
+				response => {
+					alert('下架成功')
+				},
+				error => {
+					console.log(error.message);
+				}
+			)
+		}
+	},
+	mutations: {
+		// 收到資料後存入state中
+		displayAPI(state, data){
+			state.tableData = data;
+		},
+	},
+	state: {
+		tableData: [],
+	},
+	getters:{
+		// 回傳tableData
+		getTableData(state){
+			return state.tableData
+		}
+	}
+}
+
 
 export default new Vuex.Store({
 	modules:{
 		art,
-		member
+		member,
+		products
 	},
 	plugins: [createPersistedState()],
 })
--- src/views/products.vue
+++ src/views/products.vue
@@ -5,8 +5,14 @@
             :WithFunc="func"
             :Checked="checked"
             :formInfo="formData.createFormInfo"
+            :useAPI="useAPI"
+            :finalCheckedArr="finalCheckedArr"
+            @refresh = "refresh()"
         ></PageNav>
-        <BackendTable :tableData='tableData' ref="beTable" @showDel='showDel'/>
+        <BackendTable 
+            :tableData='tableData' 
+            ref="beTable" 
+            @showDel='showDel'/>
         <FormModal :formInfo="formData.updateFormInfo" />
         <DataFooter
             :start="DataStart"
@@ -59,20 +65,20 @@ export default {
                 },
                 // 表頭名稱
                 tableHeadTitle: {
-                    IN_IMG_1: "封面",
-                    PRODUCTS_ID: "商品編號",
-                    SERIES: "商品系列",
-                    NAME: "商品名稱",
-                    PRIZE: "價格",
-                    DIAMETER: "直徑(mm)",
-                    HEIGHT: "高度(mm)",
-                    COUNT: "數量",
-                    STATUS_TYPE: "作品上架",
+                    WORKS_IN_IMG_1: "封面",
+                    WORKS_ID: "商品編號",
+                    WORKS_TYPE_ID: "商品系列",
+                    WORKS_NAME: "商品名稱",
+                    WORKS_PRICE: "價格",
+                    WORKS_RADIUS: "直徑(mm)",
+                    WORKS_HEIGHT: "高度(mm)",
+                    WORKS_STOCK: "數量",
+                    WORKS_STATUS_TYPE: "作品上架",
                     space: ""
                 },
                 // 表的內容
-                tableBodyData: this.$store.getters['art/getTableData']
-
+                // tableBodyData: this.$store.getters['products/getTableData']
+                tableBodyData: []
             },
 
             // form相關
@@ -112,27 +118,51 @@ export default {
                 imgUpload: "上傳作品圖片",
             },
 
-            // useAPI:{
-            //     displayAPI: 'art/artDisplay.php',
-            //     deleteAPI: 'art/artDelete.php',
-            // }, 
+            // 儲存有勾選tr的id
+            finalCheckedArr:[],
+
+
+
+            useAPI:{
+                displayAPI: 'products/productsDisplay.php',
+                deleteAPI: 'products/productsDelete.php',
+                upStatusAPI: 'products/productsUpStatus.php',
+                downStatusAPI: 'products/productsDownStatus.php',
+
+            }, 
         };
     },
-    methods: {
+methods: {
         // 上下架刪除按鈕是否顯示功能
         showDel(checkedArr){
+            // 儲存要刪除ID的陣列
+            this.finalCheckedArr = checkedArr;
             // 存取有勾選的陣列長度 > 0 出現
             if(checkedArr.length > 0){
                 this.checked = true
             }else{
                 this.checked = false
             }
+        },
+        refresh(){
+            this.$store.dispatch('products/displayAPI', this.useAPI.displayAPI).then(() => {
+                this.tableData.tableBodyData = this.$store.getters['products/getTableData']
+                console.log('refe');
+            })
+        }
+    },
+    watch:{
+        tableData:{
+            deep:true,
+            handler(){
+                this.$refs.beTable.tableBodyData = this.tableData.tableBodyData
+            }
         }
     },
     created(){
         // 發出ajax請求
-        this.$store.dispatch('art/displayAPI', this.useAPI.displayAPI);
-        // this.$store.dispatch('art/displayAPI', this.useAPI, this.$refs.);
+        // this.$store.dispatch('art/displayAPI', this.useAPI.displayAPI);
+        this.refresh();
     },
     mounted() {
         // 傳給th
